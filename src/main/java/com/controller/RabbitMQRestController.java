a5173ec7f0136090cdae5550e243e8be2a398378
garlam <garlam_au@qq.com>
2021-05-08 01:19:54 +0800

    update

--- src/main/java/com/controller/RabbitMQRestController.java
+++ src/main/java/com/controller/RabbitMQRestController.java
@@ -1,15 +1,20 @@
 package com.controller;
 
+import com.config.RabbitMQConfig;
 import com.entity.dao.hibernate.TestEntity;
 import com.entity.dao.hibernate.UserEntity;
+import com.mq.rabbit.RabbitReceiver;
 import com.mq.rabbit.RabbitSender;
 import com.sampleDataGenerator.DataGenerator;
 import com.service.TestService;
 import io.swagger.annotations.Api;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -30,19 +35,29 @@ public class RabbitMQRestController {
     @Autowired
     private RabbitSender rabbitSender;
     @Autowired
+    private RabbitReceiver rabbitReceiver;
+    @Autowired
     TestService testService;
 
-    public String send() throws Exception {
-        List<TestEntity> ls = DataGenerator.getTestEntity();
-        testService.saveAll(ls);
-        List<TestEntity> tlist = testService.getAll();
-        log.info("FROM MARIA DB: {}", tlist.size());
+    @Autowired
+    private AmqpTemplate amqpTemplate;
 
-        for (int i = 0; i < 6; i++) {
-            rabbitSender.send(tlist, null);
+    @RequestMapping(method = RequestMethod.GET, value = "direct/send")
+    public void sendDirectQueue() {
+        String ace = "[ ACE ]";
+
+        for (int i = 0; i < 15; i++) {
+            log.info(ace + "  " + "version::" + i);
+            this.amqpTemplate.convertAndSend(RabbitMQConfig.QUEUE, ace + "  " + "version::" + i);
         }
 
-        return "send: " + tlist.size();
+    }
+
+
+    // queues是指要监听的队列的名字
+    @RabbitListener(queues = RabbitMQConfig.QUEUE)
+    public void receiverDirectQueue(String msg) {
+        log.info("【receiverDirectQueue监听到消息】" + msg);
     }
 
 }
