ed864e3916458392aa40cbb090be80b1de136b6f
garlam <garlam_au@qq.com>
2021-05-10 01:13:44 +0800

    update

--- src/main/java/com/Ace.java
+++ src/main/java/com/Ace.java
@@ -4,22 +4,23 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import util.FileUtil;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Ace {
     private static Logger log = LogManager.getLogger(Ace.class.getName());
-   // private final static Log log = LogFactory.getLog(Ace.class);
+    // private final static Log log = LogFactory.getLog(Ace.class);
 
-    public static void main(String[] args) {
-        log.traceEntry();
-        log.trace("我是trace");
-        log.info("我是info信息:{}","ACE_UTIL");
-        log.error("我是error");
-        log.fatal("我是fatal");
+    public static void main(String[] args) throws Exception {
+        File file = new File(".");
+        File files = new File("file/image/txt.txt");
+        System.out.println("当前jvm虚拟机启动位置的绝对路径为：" + file.getAbsoluteFile());
+        System.out.println("当前jvm虚拟机启动位置的绝对路径为：" + file.getPath());
 
-        log.trace("退出程序.");
-        log.traceExit();
+        System.out.println(files.getAbsolutePath());
+        System.out.println(files.getPath());
     }
 }
