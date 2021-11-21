595e33ce19d01c2acf71f05e8b74d728db3e35f9
garlam <garlam_au@qq.com>
2021-11-21 21:37:10 +0800

    update

--- src/main/java/com/Ace.java
+++ src/main/java/com/Ace.java
@@ -1,32 +1,12 @@
 package com;
 
-import com.google.gson.Gson;
-import com.models.entity.dao.Menu;
-import net.sf.jasperreports.engine.util.JsonUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import util.FileUtil;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 
 public class Ace {
     private static Logger log = LogManager.getLogger(Ace.class.getName());
 
 
-    public static void main(String[] args) throws IOException {
-        System.out.println(System.getProperty("user.dir"));
-
-        File directory = new File("");//参数为空
-        String courseFile = directory.getCanonicalPath() ;
-        System.out.println(courseFile);
-    }
 
 }
