861184c5dacb57bdaff9a2f6c6e18946d9b45217
garlam <garlam_au@qq.com>
2021-11-26 18:02:21 +0800

    update on windows

--- src/main/java/com/controller/RolesController.java
+++ src/main/java/com/controller/RolesController.java
@@ -2,12 +2,17 @@ package com.controller;
 
 
 import com.controller.common.CommonController;
+import com.models.entity.dao.Roles;
+import com.service.RolesService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 
 /**
@@ -22,9 +27,20 @@ import org.springframework.web.servlet.ModelAndView;
 public class RolesController extends CommonController {
 	private static Logger log = LogManager.getLogger(RolesController.class.getName());
 
+	private RolesService rolesService;
+
+	@Autowired
+	public RolesController(RolesService rolesService) {
+		this.rolesService = rolesService;
+	}
+
 	@RequestMapping(value = "/roles.html", method = RequestMethod.GET)
-	public ModelAndView roles() {
+	public ModelAndView getRoleList() {
+
+		List<Roles> rolesList = rolesService.findAll();
+
 		ModelAndView modelAndView = super.page("ace/modules/roles/roles");
+		modelAndView.addObject("roles",rolesList);
 		return modelAndView;
 	}
 }
