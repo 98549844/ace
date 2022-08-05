1ca8422ada144a938aebeb953c4abd78dc9c4ee6
Kalam <kalam_au@kinetix.com>
2022-08-05 18:39:25 +0800

    update

--- src/main/java/com/util/BeanUtil.java
+++ src/main/java/com/util/BeanUtil.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.web.context.ContextLoader;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletContext;
@@ -45,6 +46,21 @@ public class BeanUtil {
 
 
     /**
+     * 从spring容器中获取bean和servletContext
+     */
+    public static void printBeanNameFromContextLoader() {
+        BeanUtil.applicationContext = ContextLoader.getCurrentWebApplicationContext();
+        String[] beanNames = BeanUtil.applicationContext.getBeanDefinitionNames();
+        log.info("total bean: {}", applicationContext.getBeanDefinitionCount());
+        // String[] beanNames = applicationContext.getBeanNamesForAnnotation(RequestMapping.class);//所有添加该注解的bean
+        int i = 0;
+        for (String s : beanNames) {
+            log.info("{},beanName: {}", ++i, s);
+        }
+    }
+
+
+    /**
      * getBean By beanName
      *
      * @param name
@@ -55,41 +71,51 @@ public class BeanUtil {
         return object;
     }
 
-    public static void main(String[] args) {
-        getXmlConfigFile();
-    }
 
     /**
-     * 加载ApplicationContext Sample
+     * 加载ApplicationContext
      *
      * @param
      */
-    public static void getXmlConfigFile() {
+    public static void getBeanFileSystemXmlApplicationContext() {
+        log.info("path pattern =>");
         //第一种: FileSystemXmlApplicationContext
         //加载单个配置文件
+        log.info("src/main/resources/xxx.xml");
         ApplicationContext ctx1 = new FileSystemXmlApplicationContext("bean.xml");
         //加载单个配置文件
         String[] locations = {"bean1.xml", "bean2.xml", "bean3.xml"};
         ApplicationContext ctx2 = new FileSystemXmlApplicationContext(locations);
         //根据具体路径加载文件
+        log.info("Absolute path => C:/xxx/xxx/xxx/xxx.xml");
         ApplicationContext ctx3 = new FileSystemXmlApplicationContext("D:/project/bean.xml");
 
     }
 
 
     /**
-     * ClassPathXmlApplicationContext --从classpath路径加载配置文件，创建Bean对象
+     * ClassPathXmlApplicationContext --从classpath路径加载配置文件
      */
-    public void getClassPathXmlApplicationContext() {
+    public Object getBeanByClassPathXmlApplicationContext() {
+        log.info("path pattern =>");
+        log.info("xxx.xml");
+        log.info("classpath:xxx.xml (location => src/main/resources/xxx.xml)");
         ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-        Object claz = (Object) ctx.getBean("beanName");
+        printBeanName(ctx);
+        return ctx.getBean("beanName");
     }
 
-    public void getApplicationContext(HttpServletRequest request) {
+    /**
+     * @param request
+     * @return
+     */
+    public Object getBeanHttpServletRequest(HttpServletRequest request) {
         HttpSession session = request.getSession();
         ServletContext context = session.getServletContext(); //arg0.getSession().getServletContext();
         ApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(context);
-        Object clazz = (Object) ctx.getBean("beanName");
+        printBeanName(ctx);
+        return ctx.getBean("beanName");
+
     }
 
 
