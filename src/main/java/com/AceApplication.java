cd170d33bcab8578790ed824bcff69dc293bb2df
Kalam <kalam_au@kinetix.com>
2022-08-05 21:51:29 +0800

    update

--- src/main/java/com/AceApplication.java
+++ src/main/java/com/AceApplication.java
@@ -59,7 +59,7 @@ public class AceApplication {
         //print host info
         BeanUtil beanUtil = new BeanUtil();
         IpUtil ip = (IpUtil) beanUtil.getBeanByName("ipUtil");
-        Map m = ip.getHostInfo();
+        Map<String, String> m = ip.getHostInfo();
         MapUtil.iterateMapKeyset(m);
 
         AceConfig aceConfig = (AceConfig) beanUtil.getBeanByName("aceConfig");
