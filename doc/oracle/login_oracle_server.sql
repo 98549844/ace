e211d76988df405122facb0f162791ce008c0e81
Kalam <kalam_au@kinetix.com>
2022-09-02 14:15:37 +0800

    update

--- doc/oracle/login_oracle_server.sql
+++ doc/oracle/login_oracle_server.sql
@@ -1,10 +1,10 @@
 --oracle 连接远程数据库
 -- 1.用sqlplus远程连接oracle命令 语法：
 sqlplus usr/pwd@//host:port/sid
-SQL*plus客户端工具：
+--SQL*plus客户端工具：
 -- 输入用户名：system/h1w2D3B4@//10.71.105.216:1526/i2kdb
 -- 或者：
-system/h1w2D3B4@i2kdb
+sqlplus system/h1w2D3B4@i2kdb
 
 
 -- 用cmd窗口中：
