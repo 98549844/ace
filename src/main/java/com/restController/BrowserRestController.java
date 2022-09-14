8842cd37f9f529afb96dda3cd7b7ef2b0942d6b0
Kalam <kalam_au@kinetix.com>
2022-09-14 12:24:49 +0800

    update

--- src/main/java/com/restController/BrowserRestController.java
+++ src/main/java/com/restController/BrowserRestController.java
@@ -1,6 +1,5 @@
 package com.restController;
 
-import com.util.OsUtil;
 import io.swagger.annotations.Api;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,13 +9,10 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.lang.reflect.Method;
 
 /**
  * @Classname: BrowserRestController
@@ -31,12 +27,13 @@ import java.lang.reflect.Method;
 public class BrowserRestController {
     private static final Logger log = LogManager.getLogger(BrowserRestController.class.getName());
 
-    @RequestMapping(method = RequestMethod.GET, value = "/get/{url}")
-    public void getUrl(@PathVariable final String url) {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/get")
+    public void getElement(@RequestParam(value = "url") String url) {
         log.info("url: {}", url);
         //如果没有配置环境变量，需要调用
         //第一个参数固定写法，第二个参数是chromedriver的安装位置
-        System.setProperty("webdriver.chrome.driver", "C:\\ideaPorject\\ace\\doc\\chrome\\windows\\chromedriver_105.exe");
+        System.setProperty("webdriver.chrome.driver", "doc/chrome/windows/chromedriver_105.exe");
         try {
             //设置不弹出浏览器
             ChromeOptions options = new ChromeOptions();
@@ -60,11 +57,11 @@ public class BrowserRestController {
 
             log.info("getElementById(ID属性)");
             Element element = document.getElementById("i_cecream");
-            log.info("element: {}", element.html());
+            log.info("element: {}", element.text());
 
             //js选择器: https://www.w3school.com.cn/js/js_jquery_selectors.asp
             Elements select = document.getElementById("i_cecream").select(".bili-feed4");
-            log.info("select: {}", select.html());
+            log.info("select: {}", select.text());
 
 
         } catch (Exception e) {
