d6c245552c2471d5884ee6458536ec08f76c7ce0
Kalam <kalam_au@kinetix.com>
2022-09-26 18:02:27 +0800

    update

--- src/main/java/com/controller/FileController.java
+++ src/main/java/com/controller/FileController.java
@@ -5,6 +5,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
 
 /**
  * @Classname: FileController
@@ -17,6 +21,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/ace")
 public class FileController extends CommonController {
     private static final Logger log = LogManager.getLogger(FileController.class.getName());
+
+    @RequestMapping(value = "/upload.html", method = RequestMethod.GET)
+    public ModelAndView upload() throws IOException {
+        System.out.println("DROPZONE .........................");
+        ModelAndView modelAndView = super.page("ace/tool-pages/gallery");
+        return modelAndView;
+    }
+
 
     public static void main(String[] args) {
         //springboot 文件上传示例    https://www.cnblogs.com/Marydon20170307/p/15794176.html
