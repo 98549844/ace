693094980cfbfd752703d826cfd5617428c83233
Kalam <kalam_au@kinetix.com>
2022-11-18 23:25:02 +0800

    update

--- src/main/java/com/controller/MobilePlayController.java
+++ src/main/java/com/controller/MobilePlayController.java
@@ -60,133 +60,22 @@ public class MobilePlayController extends CommonController {
     }
 
 
-    // getOutInputStream Exception
-    @RequestMapping(value = "/stream/mono/play.html/{ext}/{playId}", method = RequestMethod.GET)
-    @ResponseBody
-    public Mono<ResponseEntity<byte[]>> mobileMonoPlay(@PathVariable(value = "playId") String playId, @PathVariable(value = "ext") String ext, @RequestHeader(value = "Range", required = false) String httpRangeList) throws IOException {
-        log.info("access stream/play.html/{}/{}", ext, playId);
-        String device = getDevice();
-        log.info("device type: {}", device);
-
-        Mono<ResponseEntity<byte[]>> result = Mono.just(mobilePlayService.prepareContent(playId, ext, httpRangeList));
-        return result;
-    }
 
     // ClientAbort Exception and IO Exception
     @GetMapping(value = "/stream/play.html/{ext}/{playId}")
-    public ResponseEntity<StreamingResponseBody> streamPlay(@PathVariable("playId") String playId, @PathVariable("ext") String ext, @RequestHeader(value = "Range", required = false) String rangeHeader) throws IOException {
+    public ResponseEntity<StreamingResponseBody> streamPlay(
+            @PathVariable("playId") String playId, @PathVariable("ext") String ext,
+            @RequestHeader(value = "Range", required = false) String rangeHeader) throws IOException {
         log.info("access stream/play.html {} ; {}", ext, playId);
         // String mediaName = "C:\\ACE\\videos\\bbb.mp4";
         String mediaName = videoPath + playId + "." + ext;
-        try {
-            StreamingResponseBody responseStream;
-            final HttpHeaders responseHeaders = new HttpHeaders();
+        return mobilePlayService.getResponseStream (mediaName, rangeHeader);
 
-            //  final Path filePath = getMediaPath(mediaName);
-            final Path filePath = Paths.get(mediaName);
-            final long fileSize = Files.size(filePath);
-
-            byte[] buffer = new byte[1024];
-            if (rangeHeader == null) {
-                responseHeaders.add(HEADER_CONTENT_TYPE, CONTENT_TYPE_MP4);
-                responseHeaders.add(HEADER_CONTENT_LENGTH, Long.toString(fileSize));
-                responseStream = os -> {
-                    try (RandomAccessFile file = new RandomAccessFile(filePath.toFile(), "r")) {
-                        long pos = 0;
-                        file.seek(pos);
-
-                        while (pos < fileSize) {
-                            file.read(buffer);
-                            os.write(buffer);
-                            pos += buffer.length;
-                        }
-
-                        os.flush();
-                        os.close();
-                    } catch (Exception ignored) {
-                        ignored.printStackTrace();
-                    }
-                };
-                return new ResponseEntity<>(responseStream, responseHeaders, HttpStatus.OK);
-            } else {
-                // Handle partial content requests
-                String[] ranges = rangeHeader.split("-");
-                long rangeStart = Long.parseLong(ranges[0].substring(6));
-                long rangeEnd;
-
-                if (ranges.length > 1) {
-                    rangeEnd = Long.parseLong(ranges[1]);
-                } else {
-                    // If range is not found then just request the whole file
-                    rangeEnd = fileSize - 1;
-                }
-
-                // Check to make sure that content range is not outside the length of the file
-                if (fileSize < rangeEnd) {
-                    rangeEnd = fileSize - 1;
-                }
-
-                log.info("Received request for byte range: {} - {}", rangeStart, rangeEnd);
-
-                String contentLength = String.valueOf((rangeEnd - rangeStart) + 1);
-                responseHeaders.add(HEADER_CONTENT_TYPE, CONTENT_TYPE_MP4);
-                responseHeaders.add(HEADER_CONTENT_LENGTH, contentLength);
-                responseHeaders.add(HEADER_ACCEPT_RANGES, "bytes");
-                responseHeaders.add(HEADER_CONTENT_RANGE, "bytes" + " " + rangeStart + "-" + rangeEnd + "/" + fileSize);
-
-                final long _rangeEnd = rangeEnd;
-                responseStream = os -> {
-                    try (RandomAccessFile file = new RandomAccessFile(filePath.toFile(), "r")) {
-                        long pos = rangeStart;
-                        file.seek(pos);
-
-                        while (pos < _rangeEnd) {
-                            file.read(buffer);
-                            os.write(buffer);
-                            pos += buffer.length;
-                        }
-
-                        os.flush();
-                    } catch (Exception ignored) {
-                        ignored.printStackTrace();
-                    }
-                };
-
-                return new ResponseEntity<>(responseStream, responseHeaders, HttpStatus.PARTIAL_CONTENT);
-            }
-        } catch (FileNotFoundException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
     }
 
-    /**
-     * Get the path to the example video in the resources directory.
-     *
-     * @param mediaName name of media file to retrieve
-     * @return path to example video in resources directory
-     * @throws FileNotFoundException
-     */
-    private Path getMediaPath(String mediaName) throws FileNotFoundException {
-        final URL mediaResource = MediaController.class.getClassLoader().getResource(mediaName);
-        log.info("URL:=>  " + mediaResource);
-        if (mediaResource != null) {
-            try {
-                return Paths.get(mediaResource.toURI());
-            } catch (URISyntaxException e) {
-                throw new FileNotFoundException();
-            }
-        }
 
-        throw new FileNotFoundException();
-    }
 
-    private static final String HEADER_CONTENT_TYPE = "Content-Type";
-    private static final String HEADER_CONTENT_LENGTH = "Content-Length";
-    private static final String HEADER_ACCEPT_RANGES = "Accept-Ranges";
-    private static final String HEADER_CONTENT_RANGE = "Content-Range";
-    private static final String CONTENT_TYPE_MP4 = "video/mp4";
+
 
 
 }
--- src/main/java/com/service/MobilePlayService.java
+++ src/main/java/com/service/MobilePlayService.java
@@ -1,18 +1,19 @@
 package com.service;
 
 import com.constant.AceEnvironment;
-import com.util.FileUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * @Classname: MobilePlayService
@@ -25,17 +26,11 @@ import java.io.*;
 public class MobilePlayService {
     private static final Logger log = LogManager.getLogger(MobilePlayService.class.getName());
 
-    //  public static final String VIDEO = "/video";
-
-    private static final String CONTENT_TYPE = "Content-Type";
-    private static final String CONTENT_LENGTH = "Content-Length";
-    private static final String VIDEO_CONTENT = "video/";
-    private static final String CONTENT_RANGE = "Content-Range";
-    private static final String ACCEPT_RANGES = "Accept-Ranges";
-    private static final String BYTES = "bytes";
-    private static final int CHUNK_SIZE = 314700;
-    //  private static final int BYTE_RANGE = 1024;
-
+    private static final String HEADER_CONTENT_TYPE = "Content-Type";
+    private static final String HEADER_CONTENT_LENGTH = "Content-Length";
+    private static final String HEADER_ACCEPT_RANGES = "Accept-Ranges";
+    private static final String HEADER_CONTENT_RANGE = "Content-Range";
+    private static final String CONTENT_TYPE_MP4 = "video/mp4";
 
     private final String videoPath;
 
@@ -43,96 +38,88 @@ public class MobilePlayService {
         this.videoPath = AceEnvironment.getVideoPath();
     }
 
-    /**
-     * Prepare the content.
-     *
-     * @param fileName String.
-     * @param fileType String.
-     * @param range    String.
-     * @return ResponseEntity.
-     */
 
-    public ResponseEntity<byte[]> prepareContent(final String fileName, final String fileType, final String range) throws IOException {
-        HttpStatus httpStatus = HttpStatus.PARTIAL_CONTENT;
+    public ResponseEntity<StreamingResponseBody> getResponseStream(String mediaName, String rangeHeader) {
         try {
-            long rangeStart = 0;
-            long rangeEnd = CHUNK_SIZE;
-            String fileKey = fileName + "." + fileType;
-            String videoFile = videoPath + fileKey;
-            log.info("Location: {}", videoFile);
-            final Long fileSize = FileUtil.getFileSize(videoFile);
-            final byte[] videoByte = readByteRangeNew(videoFile, rangeStart, rangeEnd);
-            if (range == null) {
-                return ResponseEntity
-                        .status(httpStatus)
-                        .header(CONTENT_TYPE, VIDEO_CONTENT + fileType)
-                        .header(ACCEPT_RANGES, BYTES)
-                        .header(CONTENT_LENGTH, String.valueOf(rangeEnd))
-                        .header(CONTENT_RANGE, BYTES + " " + rangeStart + "-" + rangeEnd + "/" + fileSize)
-                        .header(CONTENT_LENGTH, String.valueOf(fileSize))
-                        .body(videoByte); // Read the object and convert it as bytes
-            }
-            String[] ranges = range.split("-");
-            rangeStart = Long.parseLong(ranges[0].substring(6));
-            if (ranges.length > 1) {
-                rangeEnd = Long.parseLong(ranges[1]);
+            StreamingResponseBody responseStream;
+            final HttpHeaders responseHeaders = new HttpHeaders();
+
+            final Path filePath = Paths.get(mediaName);
+            final long fileSize = Files.size(filePath);
+
+            byte[] buffer = new byte[1024];
+            if (rangeHeader == null) {
+                responseHeaders.add(HEADER_CONTENT_TYPE, CONTENT_TYPE_MP4);
+                responseHeaders.add(HEADER_CONTENT_LENGTH, Long.toString(fileSize));
+                responseStream = os -> {
+                    try (RandomAccessFile file = new RandomAccessFile(filePath.toFile(), "r")) {
+                        long pos = 0;
+                        file.seek(pos);
+
+                        while (pos < fileSize) {
+                            file.read(buffer);
+                            os.write(buffer);
+                            pos += buffer.length;
+                        }
+
+                        os.flush();
+                        os.close();
+                    } catch (Exception ignored) {
+                        ignored.printStackTrace();
+                    }
+                };
+                return new ResponseEntity<>(responseStream, responseHeaders, HttpStatus.OK);
             } else {
-                rangeEnd = rangeStart + CHUNK_SIZE;
+                // Handle partial content requests
+                String[] ranges = rangeHeader.split("-");
+                long rangeStart = Long.parseLong(ranges[0].substring(6));
+                long rangeEnd;
+
+                if (ranges.length > 1) {
+                    rangeEnd = Long.parseLong(ranges[1]);
+                } else {
+                    // If range is not found then just request the whole file
+                    rangeEnd = fileSize - 1;
+                }
+
+                // Check to make sure that content range is not outside the length of the file
+                if (fileSize < rangeEnd) {
+                    rangeEnd = fileSize - 1;
+                }
+
+                log.info("Received request for byte range: {} - {}", rangeStart, rangeEnd);
+
+                String contentLength = String.valueOf((rangeEnd - rangeStart) + 1);
+                responseHeaders.add(HEADER_CONTENT_TYPE, CONTENT_TYPE_MP4);
+                responseHeaders.add(HEADER_CONTENT_LENGTH, contentLength);
+                responseHeaders.add(HEADER_ACCEPT_RANGES, "bytes");
+                responseHeaders.add(HEADER_CONTENT_RANGE, "bytes" + " " + rangeStart + "-" + rangeEnd + "/" + fileSize);
+
+                final long _rangeEnd = rangeEnd;
+                responseStream = os -> {
+                    try (RandomAccessFile file = new RandomAccessFile(filePath.toFile(), "r")) {
+                        long pos = rangeStart;
+                        file.seek(pos);
+
+                        while (pos < _rangeEnd) {
+                            file.read(buffer);
+                            os.write(buffer);
+                            pos += buffer.length;
+                        }
+
+                        os.flush();
+                    } catch (Exception ignored) {
+                        ignored.printStackTrace();
+                    }
+                };
+                return new ResponseEntity<>(responseStream, responseHeaders, HttpStatus.PARTIAL_CONTENT);
             }
-
-            rangeEnd = Math.min(rangeEnd, fileSize - 1);
-            final byte[] data = readByteRangeNew(videoFile, rangeStart, rangeEnd);
-            final String contentLength = String.valueOf((rangeEnd - rangeStart) + 1);
-            if (rangeEnd >= fileSize) {
-                httpStatus = HttpStatus.OK;
-            }
-            ResponseEntity responseEntity = ResponseEntity
-                    .status(httpStatus)
-                    .header(CONTENT_TYPE, VIDEO_CONTENT + fileType)
-                    .header(ACCEPT_RANGES, BYTES).header(CONTENT_LENGTH, contentLength)
-                    .header(CONTENT_RANGE, BYTES + " " + rangeStart + "-" + rangeEnd + "/" + fileSize)
-                    .body(data);
-            return responseEntity;
-        } catch (IOException e) {
-            log.error("Exception while reading the file {}", e.getMessage());
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
-
-    /**
-     * ready file byte by byte.
-     *
-     * @param videoFile String.
-     * @param start     long.
-     * @param end       long.
-     * @return byte array.
-     * @throws IOException exception.
-     */
-    public byte[] readByteRangeNew(String videoFile, long start, long end) throws IOException {
-
-        InputStream input = null;
-        byte[] data = null;
-        try {
-            File file = new File(videoFile);
-            input = new FileInputStream(file);
-
-            data = new byte[input.available()];
-
-            input.read(data);
         } catch (FileNotFoundException e) {
-            log.info("file not find!");
-            e.printStackTrace();
-        } catch (IOException e) {
-            log.info("IOException :" + e);
-            e.printStackTrace();
-        } finally {
-            assert input != null;
-            input.close();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        byte[] result = new byte[(int) (end - start) + 1];
-        System.arraycopy(data, (int) start, result, 0, (int) (end - start) + 1);
-        return result;
+
     }
 
 
