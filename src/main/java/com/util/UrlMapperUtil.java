7618af83ac935723a9996bb2bdc9b9f7b751f9ee
Kalam <kalam_au@kinetix.com>
2023-01-15 00:27:25 +0800

    update

--- src/main/java/com/util/UrlMapperUtil.java
+++ src/main/java/com/util/UrlMapperUtil.java
@@ -28,12 +28,12 @@ public class UrlMapperUtil implements ApplicationContextAware {
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-       // Map<String, Object> controllerBeans = applicationContext.getBeansWithAnnotation(Controller.class);
-        Map<String, Object> restControllerBeans = applicationContext.getBeansWithAnnotation(RestController.class);
+        Map<String, Object> controllerBeans = applicationContext.getBeansWithAnnotation(Controller.class);
+       // Map<String, Object> restControllerBeans = applicationContext.getBeansWithAnnotation(RestController.class);
 
         List<String> list = new ArrayList<>();
-       // list.addAll(getUrl(controllerBeans, applicationContext));
-        list.addAll(getUrl(restControllerBeans, applicationContext));
+        list.addAll(getUrl(controllerBeans, applicationContext));
+       // list.addAll(getUrl(restControllerBeans, applicationContext));
 
     }
 
