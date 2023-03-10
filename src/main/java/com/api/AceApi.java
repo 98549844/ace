e92582d956e5bc2be7618b82da2ac70ebc99062c
Kalam <kalam_au@kinetix.com>
2023-03-10 17:25:20 +0800

    update

--- src/main/java/com/api/AceApi.java
+++ src/main/java/com/api/AceApi.java
@@ -1,23 +1,23 @@
-//package com.api;
-//
-//import com.models.common.AjaxResponse;
-//import org.springframework.cloud.openfeign.FeignClient;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-///**
-// * @Classname: AceApi
-// * @Date: 9/5/2021 9:52 上午
-// * @Author: garlam
-// * @Description:
-// */
-//
-//
-//@FeignClient(name = "aceJob", url = "http://localhost:8088/")
-//public interface AceApi {
-//
-//
-//    //call target controller url
-//    @GetMapping("/rest/users/getUsers")
-//    AjaxResponse getAllUsers();
-//
-//}
+package com.api;
+
+import com.models.common.AjaxResponse;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+
+/**
+ * @Classname: AceApi
+ * @Date: 9/5/2021 9:52 上午
+ * @Author: garlam
+ * @Description:
+ */
+
+
+
+@HttpExchange("/rest")
+public interface AceApi {
+
+    //call target controller url
+    @GetExchange("/users/getUsers")
+    AjaxResponse getAllUsers();
+
+}
--- src/main/java/com/restController/ApiRestController.java
+++ src/main/java/com/restController/ApiRestController.java
@@ -1,46 +1,48 @@
-//package com.restController;
-//
-//import com.api.AceApi;
-//import com.api.Response;
-//import com.models.common.AjaxResponse;
-//import io.swagger.annotations.Api;
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-///**
-// * @Classname: ApiRestController
-// * @Date: 9/5/2021 10:04 上午
-// * @Author: garlam
-// * @Description:
-// */
-//
-//@RestController
-//@RequestMapping("/rest/api")
+package com.restController;
+
+import com.api.AceApi;
+import com.api.Response;
+import com.models.common.AjaxResponse;
+import com.models.entity.Users;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @Classname: ApiRestController
+ * @Date: 9/5/2021 10:04 上午
+ * @Author: garlam
+ * @Description:
+ */
+
+@RestController
+@RequestMapping("/rest/api")
 //@Api(tags = "api")
-//public class ApiRestController {
-//    private static final Logger log = LogManager.getLogger(ApiRestController.class.getName());
-//
-//    private final AceApi aceApi;
-//
-//    @Autowired
-//    public ApiRestController(AceApi aceApi) {
-//
-//        this.aceApi = aceApi;
-//    }
-//
-//    @GetMapping("/getAllUsers")
-//    public Response getAllUsers() {
-//        AjaxResponse ajaxResponse = aceApi.getAllUsers();
-//        log.info(ajaxResponse);
-//        return Response.success(ajaxResponse);
-//        // return Response.success(null);
-//
-//    }
-//
-//
-//}
-//
+@Tag(name = "Api")
+public class ApiRestController {
+    private static final Logger log = LogManager.getLogger(ApiRestController.class.getName());
+
+    private final AceApi aceApi;
+
+    @Autowired
+    public ApiRestController(AceApi aceApi) {
+
+        this.aceApi = aceApi;
+    }
+
+    @GetMapping("/getAllUsers")
+    public Response getAllUsers() {
+        AjaxResponse ajaxResponse = aceApi.getAllUsers();
+        log.info(ajaxResponse.getData());
+        return Response.success(ajaxResponse);
+    }
+
+
+}
+
