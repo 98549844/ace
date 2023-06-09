2efcb1f785a4fb7a8fc3d28fe4439c21d86e064c
Kalam <kalam_au@kinetix.com>
2023-06-09 12:19:13 +0800

    update

--- src/main/java/com/ace/Ace.java
+++ src/main/java/com/ace/Ace.java
@@ -20,15 +20,12 @@ public class Ace {
     private static final Logger log = LogManager.getLogger(Ace.class.getName());
 
     public static void main(String[] args) throws UnknownHostException {
-//        log.error( java.net.getByName("api.coinmarketcap.com" ) );
-        IpUtil.getDomainIp("api.coinmarketcap.com");
-        System.out.println(IpUtil.getHostName());
 
-        String ip = InetAddress.getLocalHost().getHostAddress();
-        System.out.println("ip: "+ip);
 
-        //  JSONObject result = RestUtil.get_StrToJson("https://13.226.120.9/data-api/v3/cryptocurrency/quote/latest?id=1&convertld=2792",10000,null);
     }
+
+
+
 
 }
 
