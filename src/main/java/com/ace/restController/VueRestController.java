3d59b4ea3003d612b71c7a22a57ce67fb2b3a39e
garlam <garlam_au@qq.com>
2023-09-26 21:46:19 +0800

    update

--- src/main/java/com/ace/restController/VueRestController.java
+++ src/main/java/com/ace/restController/VueRestController.java
@@ -20,13 +20,13 @@ import java.util.List;
 @CrossOrigin // 解决浏览器禁止ajax请求本地以外的资源, 后端同时在Controller层的类上增加@CrossOrign注解
 @RestController
 @RequestMapping("/rest/vue")
-@Tag(name = "Ace")
+@Tag(name = "Vue")
 public class VueRestController {
     private static final Logger log = LogManager.getLogger(VueRestController.class.getName());
 
 
-    @RequestMapping(method = RequestMethod.GET, value = "/get")
-    public AjaxResponse getAceProperties() {
+    @RequestMapping(method = RequestMethod.GET, value = "/get.html")
+    public AjaxResponse getAceVueProperties() {
         List aceList = new ArrayList();
         aceList.add("[ ACE ]");
         aceList.add("version 2.5");
@@ -34,16 +34,16 @@ public class VueRestController {
         return AjaxResponse.success(aceList);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/getAce")
+    @RequestMapping(method = RequestMethod.GET, value = "/getAce.html")
     public String getAce() {
         String result = "[ ACE ]" + "   " + "version 2.5";
         System.out.println(result);
         return result;
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/formData")
+    @RequestMapping(method = RequestMethod.POST, value = "/formData.html")
     public AjaxResponse text(@RequestBody FormData formData) {
-        String ace = "[ ACE ]"+" ";
+        String ace = "[ ACE ]" + " ";
         String name = formData.getName() + " ";
         String email = formData.getEmail() + " ";
         String result = ace + name + email;
