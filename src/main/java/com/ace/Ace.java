32728d68ae3483cbd1fb83776037dba6047dc541
Kalam <kalam_au@kinetix.com>
2023-10-04 14:52:35 +0800

    update

--- src/main/java/com/ace/Ace.java
+++ src/main/java/com/ace/Ace.java
@@ -1,103 +1,31 @@
 package com.ace;
 
-import java.util.*;
+import com.util.FileUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+/**
+ * @Classname: Ace
+ * @Date: 2023/10/4 下午 02:15
+ * @Author: kalam_au
+ * @Description:
+ */
+
 
 public class Ace {
+    private static final Logger log = LogManager.getLogger(Ace.class.getName());
+
+    final static String p = "C:\\ideaPorject\\zfile-vue\\dist\\assets";
+
+    public static void main(String[] args) throws IOException {
+
+          FileUtil.countByType(p);
 
 
-    public static void main(String[] args) {
-        List<Node> nodes = generateNodes();
 
-        Map<Integer, Node> nodeMap = new HashMap<>();
-        Node root = null;
-
-        // 构建节点映射表
-        for (Node node : nodes) {
-            nodeMap.put(node.getId(), node);
-        }
-
-        // 构建树状结构
-        for (Node node : nodes) {
-            int parentId = node.getParentId();
-            if (parentId == 0) {
-                root = node;
-            } else {
-                Node parent = nodeMap.get(parentId);
-                if (parent != null) {
-                    parent.addChild(node);
-                }
-            }
-        }
-
-        // 打印树状结构
-        if (root != null) {
-            printTree(root, 0);
-        }
     }
 
-    private static List<Node> generateNodes() {
-        List<Node> nodes = new ArrayList<>();
-
-        // 添加节点数据
-        nodes.add(new Node(1, 0, "Node 1"));
-        nodes.add(new Node(2, 1, "Node 2"));
-        nodes.add(new Node(3, 1, "Node 3"));
-        nodes.add(new Node(4, 2, "Node 4"));
-        nodes.add(new Node(5, 2, "Node 5"));
-        nodes.add(new Node(6, 3, "Node 6"));
-
-        return nodes;
-    }
-
-    private static void printTree(Node node, int level) {
-        StringBuilder indent = new StringBuilder();
-        for (int i = 0; i < level; i++) {
-            indent.append("  ");
-        }
-
-        System.out.println(indent + node.getName());
-
-        List<Node> children = node.getChildren();
-        for (Node child : children) {
-            printTree(child, level + 1);
-        }
-    }
-
-    static class Node {
-        private int id;
-        private int parentId;
-        private String name;
-        private List<Node> children;
-
-        public Node(int id, int parentId, String name) {
-            this.id = id;
-            this.parentId = parentId;
-            this.name = name;
-            this.children = new ArrayList<>();
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public int getParentId() {
-            return parentId;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public List<Node> getChildren() {
-            return children;
-        }
-
-        public void addChild(Node child) {
-            children.add(child);
-        }
-    }
 }
-
-
-
 
