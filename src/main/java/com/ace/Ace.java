e7d30bc71fdf831afe50026b9e5918fa4df1622a
Kalam <kalam_au@kinetix.com>
2023-10-05 16:23:12 +0800

    update

--- src/main/java/com/ace/Ace.java
+++ src/main/java/com/ace/Ace.java
@@ -17,11 +17,13 @@ import java.io.IOException;
 public class Ace {
     private static final Logger log = LogManager.getLogger(Ace.class.getName());
 
-    final static String p = "C:\\ideaPorject\\zfile-vue\\dist\\assets";
+    final static String p = "C:\\ideaPorject\\zfile\\src\\main\\resources\\static\\aaa\\";
+//    final static String p = "C:\\ace\\tmp";
 
     public static void main(String[] args) throws IOException {
 
           FileUtil.countByType(p);
+          FileUtil.count(p);
 
 
 
