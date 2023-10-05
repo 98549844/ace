3148698e596cf39ce0a1034be15581d0ac35987f
garlam <garlam_au@qq.com>
2023-10-05 23:47:39 +0800

    update

--- src/main/java/com/ace/Ace.java
+++ src/main/java/com/ace/Ace.java
@@ -25,11 +25,11 @@ import java.util.Date;
 public class Ace {
     private static final Logger log = LogManager.getLogger(Ace.class.getName());
 
-   // final static String p = "/Users/garlam/IdeaProjects/ace/src/main/java/com/ace/Ace.java";
-      final static String p = "/Users/garlam/ace/images/6c1aa51c-0a59-4dcb-8c18-8ac655e79a1f.jpeg";
+    // final static String p = "/Users/garlam/IdeaProjects/ace/src/main/java/com/ace/Ace.java";
+    final static String p = "/Users/garlam/ace/images/6c1aa51c-0a59-4dcb-8c18-8ac655e79a1f.jpeg";
 
-    public static void main(String[] args) throws IOException {
-// 指定自己的目标文件
+    public static void getCreateDate() throws IOException {
+        // 指定自己的目标文件
         File file = new File(p);
         // 根据文件的绝对路径获取Path
         Path path = Paths.get(file.getAbsolutePath());
@@ -47,5 +47,51 @@ public class Ace {
         System.out.println(time);
     }
 
+    public static void getLastAccessDate() throws IOException {
+        // 指定自己的目标文件
+        File file = new File(p);
+        // 根据文件的绝对路径获取Path
+        Path path = Paths.get(file.getAbsolutePath());
+        // 根据path获取文件的基本属性类
+        BasicFileAttributes attrs = Files.readAttributes(path, BasicFileAttributes.class);
+        // 从基本属性类中获取文件创建时间
+        FileTime fileTime = attrs.lastAccessTime();
+        // 将文件创建时间转成毫秒
+        long millis = fileTime.toMillis();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
+        Date date = new Date();
+        date.setTime(millis);
+        // 毫秒转成时间字符串
+        String time = dateFormat.format(date);
+        System.out.println(time);
+    }
+
+    public static void getLastModifiedDate() throws IOException {
+        // 指定自己的目标文件
+        File file = new File(p);
+        // 根据文件的绝对路径获取Path
+        Path path = Paths.get(file.getAbsolutePath());
+        // 根据path获取文件的基本属性类
+        BasicFileAttributes attrs = Files.readAttributes(path, BasicFileAttributes.class);
+        // 从基本属性类中获取文件创建时间
+        FileTime fileTime = attrs.lastModifiedTime();
+        // 将文件创建时间转成毫秒
+        long millis = fileTime.toMillis();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
+        Date date = new Date();
+        date.setTime(millis);
+        // 毫秒转成时间字符串
+        String time = dateFormat.format(date);
+        System.out.println(time);
+    }
+
+    public static void main(String[] args) throws IOException {
+        getLastModifiedDate();
+        getLastAccessDate();
+        getCreateDate();
+    }
+
+    //读取文件大小
+    //https://blog.csdn.net/cn_lyg/article/details/103784912/
 }
 
