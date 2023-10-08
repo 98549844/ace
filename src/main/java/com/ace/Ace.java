9609af6fdd9ecf3de85d2e24fd4cbea6ff656d8a
garlam <garlam_au@qq.com>
2023-10-08 19:50:35 +0800

    update

--- src/main/java/com/ace/Ace.java
+++ src/main/java/com/ace/Ace.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Classname: Ace
@@ -21,13 +22,16 @@ public class Ace {
         String s = "/Users/garlam/IdeaProjects/ace/src/main/resources/static/assets";
         // String s = "/Users/garlam/IdeaProjects/ace/src/main/resources/static/assets/fonts";
         FileUtil f = new FileUtil();
-        List ls = f.getFilePaths(s);
+       /* List ls = f.getFilePaths(s);
         List list = FileUtil.getFolderAndSubFolderList(s);
 
         for (Object location : list) {
             log.info(location.toString());
             FileUtil.countByType(location.toString());
-        }
+        }*/
+        Map a = f.countByTypeWithSubFolders(s);
+        System.out.println("-----");
+
     }
 }
 
