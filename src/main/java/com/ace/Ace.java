0fcf3022c2f2629e528f4fcb52565b8d8b8ac8e8
garlam <garlam_au@qq.com>
2024-01-17 19:19:51 +0800

    update

--- src/main/java/com/ace/Ace.java
+++ src/main/java/com/ace/Ace.java
@@ -19,35 +19,6 @@ import java.io.*;
 public class Ace {
     private static final Logger log = LogManager.getLogger(Ace.class.getName());
 
-    public static void main1(String[] args) {
-        String p = System.getProperty("user.dir") + "/src/main/java/com/ace/";
-        String name = "result.txt";
-        String aaa = "ggg" + PathUtil.getNewLine();
-        // 从 0.0.0.0 到 255.255.255.255
-        System.out.println("start");
-        StringBuilder re = new StringBuilder();
-        for (int i = 0; i <= 10; i++) {
-            for (int j = 0; j <= 40; j++) {
-                for (int k = 0; k <= 180; k++) {
-                    for (int l = 0; l <= 255; l++) {
-                       // String ipv4Address = i + "." + j + "." + k + "." + l;
-                        String ipv4Address = l + "." + k + "." + j + "." + i;
-                        String region = RegionUtil.getAddr(ipv4Address);
-                        if (!region.contains("内网IP")) {
-                            re.append(region).append(PathUtil.getNewLine());
-                        }
-                    }
-                }
-            }
-        }
-        FileUtil.write(p, name, re, true);
-        System.out.println("end");
 
-    }
-//nBRNFqr99SYc
-    public static void main(String[] args) {
-        String a = RegionUtil.getAddr("120.235.14.222");
-        System.out.println(a);
-    }
 }
 
