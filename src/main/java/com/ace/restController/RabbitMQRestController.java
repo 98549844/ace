165f18ac66cf400afeed55e528a89dc9b5aa6f02
garlam <garlam_au@qq.com>
2024-02-24 20:03:33 +0800

    update

--- src/main/java/com/ace/restController/RabbitMQRestController.java
+++ src/main/java/com/ace/restController/RabbitMQRestController.java
@@ -3,12 +3,16 @@ package com.ace.restController;
 import com.ace.config.RabbitMQConfig;
 import com.ace.models.entity.Users;
 import com.ace.service.UsersService;
+import com.rabbitmq.client.Channel;
 import com.util.Console;
+import com.util.FastJson2Util;
+import com.util.ObjectUtil;
 import com.util.SleepUtil;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,7 +44,7 @@ public class RabbitMQRestController {
         this.amqpTemplate = amqpTemplate;
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "direct/send")
+    @RequestMapping(method = RequestMethod.GET, value = "direct/send/string")
     public void sendDirectQueue() {
         String ace = "[ ACE ]";
         for (int i = 0; i < 10; i++) {
@@ -50,13 +54,22 @@ public class RabbitMQRestController {
         }
     }
 
-   // @RequestMapping(method = RequestMethod.GET, value = "direct/send/user")
+    @RequestMapping(method = RequestMethod.GET, value = "direct/send/user")
     public void sendDirectUser() {
         List<Users> users = usersService.findAll();
         for (Users u : users) {
-            // 监听类型转换失败
-            //https://blog.csdn.net/m0_46360888/article/details/130268349
             this.amqpTemplate.convertAndSend(RabbitMQConfig.QUEUE_USERS, u);
+            SleepUtil.sleep(1);
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "direct/send/message")
+    public void sendDirectMessage() {
+        List<Users> users = usersService.findAll();
+        for (Users u : users) {
+            //object转换成byte[]
+            Message msg = new Message(ObjectUtil.toByteArray(u));
+            this.amqpTemplate.convertAndSend(RabbitMQConfig.QUEUE_MESSAGE, msg);
             SleepUtil.sleep(1);
         }
     }
@@ -66,23 +79,33 @@ public class RabbitMQRestController {
     @RabbitListener(queues = RabbitMQConfig.QUEUE)
     public void receiverDirectQueue1(String msg) {
         SleepUtil.sleep(3);
-        Console.println("【receiver监听到消息1】" + msg, Console.BOLD);
+        Console.println(RabbitMQConfig.QUEUE + "【监听到消息1】" + msg, Console.BOLD);
     }
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE)
     public void receiverDirectQueue2(String msg) {
         SleepUtil.sleep(3);
-        Console.println("【receiver监听到消息2】" + msg, Console.BOLD);
+        Console.println(RabbitMQConfig.QUEUE + "【监听到消息2】" + msg, Console.BOLD);
     }
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE_USERS)
-    public void receiverDirectQueue(Users users) {
-        // 监听类型转换失败
-        //https://blog.csdn.net/m0_46360888/article/details/130268349
+    public void receiverUsers(Users users) {
         SleepUtil.sleep(3);
-        Console.println("【receiverDirectQueue监听到消息】" + users.getUsername(), Console.BOLD);
-
+        Console.println(RabbitMQConfig.QUEUE_USERS + "【监听到消息】" + users.getUsername(), Console.BOLD);
     }
 
-}
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_MESSAGE)
+    public void receiverMessage(Channel channel, Message msg) {
+        try {
+            long msgTag = msg.getMessageProperties().getDeliveryTag();
+            Users users=  FastJson2Util.BytesArrayToObject(msg.getBody(),Users.class);
+            System.out.println(msgTag + ". " + RabbitMQConfig.QUEUE_MESSAGE + "【监听到消息】" + users.getUsername());
+            // 手动确认,只确认当条消息, 默认是自动确认
+            // channel.basicAck(msgTag, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+
+}
