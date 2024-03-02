6e1216fbf9843ccdaac9ab15640baaba354f85f8
garlam <garlam_au@qq.com>
2024-03-02 23:57:42 +0800

    update

---
+++ src/main/java/com/ace/restController/IpAddressRestController.java
@@ -0,0 +1,44 @@
+package com.ace.restController;
+
+import com.ace.api.Response;
+import com.ace.models.common.AjaxResponse;
+import com.ace.util.IpUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import com.ace.controller.common.CommonController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @Classname: IpAddressRestController
+ * @Date: 2/3/2024 11:51 pm
+ * @Author: garlam
+ * @Description:
+ */
+
+@RestController
+@RequestMapping("/rest/ip")
+@Tag(name = "Ip Address")
+public class IpAddressRestController extends CommonController {
+    private static final Logger log = LogManager.getLogger(IpAddressRestController.class.getName());
+
+    private final IpUtil ipUtil;
+
+    public IpAddressRestController(IpUtil ipUtil) {
+        this.ipUtil = ipUtil;
+    }
+
+    @GetMapping("/getLocation/{ip}")
+    @Operation(summary = "Get location by Ip")
+    public Response getIpLocationInfo(@PathVariable String ip) {
+        String ipLocal = ipUtil.getLocalCityInfo(ip);
+        return Response.success(ipLocal);
+    }
+
+}
+
