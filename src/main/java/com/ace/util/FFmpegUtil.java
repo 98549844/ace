9bca0f6837e733eef772f6311be82ece99a7737b
garlam <garlam_au@qq.com>
2024-03-05 01:36:55 +0800

    update

--- src/main/java/com/ace/util/FFmpegUtil.java
+++ src/main/java/com/ace/util/FFmpegUtil.java
@@ -83,7 +83,7 @@ public class FFmpegUtil {
 
         // key_info 文件写入
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("key").append(newLine);                  // m3u8加载key文件网络路径
+        stringBuilder.append("key").append(newLine);                    // m3u8加载key文件网络路径
         stringBuilder.append(keyFile).append(newLine);                  // FFmpeg加载key_info文件路径
         stringBuilder.append(iv);                                       // ASE 向量
 
