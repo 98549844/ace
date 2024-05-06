45098a6abeac18e1edeef9f278c24f3fb10ee200
garlam <garlam_au@qq.com>
2024-05-06 08:57:30 +0800

    update

--- src/main/java/com/ace/Ace.java
+++ src/main/java/com/ace/Ace.java
@@ -1,7 +1,6 @@
 package com.ace;
 
 import com.ace.util.CangJieUtil;
-import com.ace.utilities.ResourcesUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,23 +20,7 @@ public class Ace {
 
     public static void main(String[] args) throws IOException {
 
-        String a = ResourcesUtil.get("application-prod.yml");
-        System.out.println(a);
 
-        //InputStream inputStream = Ace.class.getResourceAsStream("/application-prod.yml");
-        //if (inputStream != null) {
-        //    // 在这里处理文件的输入流
-        //    // 例如，使用 BufferedReader 读取文本文件
-        //    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        //    String line;
-        //    while ((line = reader.readLine()) != null) {
-        //        System.out.println(line);
-        //    }
-        //    reader.close();
-        //
-        //} else {
-        //    System.out.println("无法找到文件！");
-        //}
     }
 
     private static void cangJieCheck(String code) throws IOException {
