df8bf2481bd89151bd402bc727bfbd34b4c16983
garlam <garlam_au@qq.com>
2024-05-12 15:32:52 +0800

    update

--- src/main/java/com/ace/restController/RRWebRestController.java
+++ src/main/java/com/ace/restController/RRWebRestController.java
@@ -4,6 +4,7 @@ import com.ace.exception.ResponseException;
 import com.ace.models.common.AjaxResponse;
 import com.ace.models.entity.AccessLog;
 import com.ace.utilities.FastJson2Util;
+import com.ace.utilities.FileUtil;
 import com.alibaba.fastjson2.JSONObject;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -34,26 +35,27 @@ import java.util.Map;
 public class RRWebRestController {
     private static final Logger log = LogManager.getLogger(RRWebRestController.class.getName());
 
-    Map<String, String> dataMap = new HashMap<>();
-    List<String> dataList = new ArrayList<>();
-    String[] token;
-    String[] body;
     String tmp;
 
     @Operation(summary = "保存记录")
     @RequestMapping(method = RequestMethod.POST, value = "/save.html")
     public AjaxResponse save(@RequestBody String data) {
         tmp = data;
+        String p = "/Users/garlam/IdeaProjects/ace/src/main/java/com/ace/restController/";
+        String f = "_data.txt";
+        FileUtil.write(p, f, data, false);
+
+
         return AjaxResponse.success(data);
     }
 
 
     @Operation(summary = "回放")
     @RequestMapping(method = RequestMethod.GET, value = "/playback/{playbackId}")
-    public String playback(@PathVariable String playbackId) {
+    public AjaxResponse playback(@PathVariable String playbackId) {
         log.info("access RRWeb playbackId: {}", playbackId);
-        // return AjaxResponse.success(tmp);
-        return tmp;
+        return AjaxResponse.success(tmp);
+        //  return tmp;
     }
 
     @Operation(summary = "回放列表")
