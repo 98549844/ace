df8f73500cf270c7735e874699d974cd4530a004
garlam.au <garlam.au@win-hanverky.com.hk>
2024-07-25 12:13:17 +0800

    update

--- alter_schema.sql
+++ alter_schema.sql
@@ -2,14 +2,16 @@ show schemas ;
 show tables;
 select version();
 
-create schema test default character set utf8mb4 collate utf8mb4_unicode_ci;#创建数据库
-grant select,insert,update,delete,create on test.* to root;#用户授权数据库
+create schema sayOrder default character set utf8mb4 collate utf8mb4_unicode_ci;#创建数据库
+grant select,insert,update,delete,create on sayOrder.* to root;#用户授权数据库
 flush privileges;#立即启用配置
 
 create user 'garlam'@'%' identified by '';
 GRANT ALL PRIVILEGES ON ace.* TO 'garlam'@'%';
 # grant select,insert,update,delete,create on ace.* to garlam;#用户授权数据库
 commit;
-drop schema ace;#删除数据库
+
+
+drop schema sayOrder;#删除数据库
 
 
